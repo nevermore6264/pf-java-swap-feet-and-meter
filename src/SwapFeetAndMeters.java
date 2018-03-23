0c8f1b2c87d4a98d96e358fbc1e86ffa9dfc2855
nevermore6264 <37317921+nevermore6264@users.noreply.github.com>
2018-03-23 16:26:42 +0700

    [Bài tập] Chuyển đổi giữa feet và meters

---
+++ src/SwapFeetAndMeters.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class SwapFeetAndMeters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        double foot, meter;
+        do {
+            System.out.println("--------------------------");
+            System.out.println("|           Menu          |");
+            System.out.println("|1. Foot To Meter |");
+            System.out.println("|2. Meter To Foot |");
+            System.out.println("|0. Exit                  |");
+            System.out.println("--------------------------");
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter number foot: ");
+                    foot = scanner.nextDouble();
+                    System.out.println("Result is : " + footToMeter(foot));
+                    break;
+                case 2:
+                    System.out.println("Enter number meter : ");
+                    meter = scanner.nextDouble();
+                    System.out.println("Result is : " + meterToFoot(meter));
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Mời bạn chọn giá trị từ 0 -2 ");
+            }
+        } while (choice != 0);
+    }
+
+    public static double footToMeter(double foot) {
+        final double FOOT_TO_MET = 0.305;
+        double meter = FOOT_TO_MET * foot;
+        return meter;
+    }
+
+    /**
+     * Chuyển đổi từ meters sang feet
+     **/
+    public static double meterToFoot(double meter) {
+        final double MET_TO_FOOT = 3.279;
+        double foot = MET_TO_FOOT * meter;
+        return foot;
+
+    }
+}
